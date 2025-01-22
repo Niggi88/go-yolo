b68dae8828efae8e0cfa5287579f671639a6dd74
Niklas Unverricht <niklas@unverricht.net>
2025-01-22 18:43:07 +0100

    add benchmark for classifier

--- bench.go
+++ bench.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"image"
 	"time"
+	"yolo_detection/classifier"
 	"yolo_detection/detector"
 	"yolo_detection/imageutils"
 )
@@ -14,7 +15,7 @@ type BenchmarkResult struct {
     NumRuns       int
 }
 
-func measureInferenceTime(yolo *detector.YOLODetector, img image.Image, runs int) (BenchmarkResult, error) {
+func measureInferenceTimeYolo(yolo *detector.YOLODetector, img image.Image, runs int) (BenchmarkResult, error) {
     var totalInferenceTime time.Duration
 
     // Do preprocessing once
@@ -34,6 +35,44 @@ func measureInferenceTime(yolo *detector.YOLODetector, img image.Image, runs int
     for i := 0; i < runs; i++ {
         inferenceStart := time.Now()
         err := yolo.RunInferenceOnly()  // Only running session.Run()
+        if err != nil {
+            return BenchmarkResult{}, fmt.Errorf("inference failed: %v", err)
+        }
+        inferenceTime := time.Since(inferenceStart)
+        totalInferenceTime += inferenceTime
+
+        // fmt.Printf("Run %d - Inference: %v\n", i+1, inferenceTime)
+    }
+
+    avgInferenceTime := totalInferenceTime / time.Duration(runs)
+    
+    return BenchmarkResult{
+        InferenceTime: avgInferenceTime,
+        NumRuns:      runs,
+    }, nil
+}
+
+
+func measureInferenceTimeBinary(model *classifier.Classifier, img image.Image, runs int) (BenchmarkResult, error) {
+    var totalInferenceTime time.Duration
+
+    // Do preprocessing once
+    targetSize := imageutils.ImageSize{
+        Width:  detector.DefaultConfig.InputWidth,
+        Height: detector.DefaultConfig.InputHeight,
+    }
+    imageutils.PreprocessImage(img, targetSize)
+    
+    // Do one full detection to set up the tensors
+    _, err := model.Classify(img)
+    if err != nil {
+        return BenchmarkResult{}, fmt.Errorf("initial detection failed: %v", err)
+    }
+
+    // Run just inference (session.Run) multiple times
+    for i := 0; i < runs; i++ {
+        inferenceStart := time.Now()
+        err := model.RunInferenceOnly()  // Only running session.Run()
         if err != nil {
             return BenchmarkResult{}, fmt.Errorf("inference failed: %v", err)
         }
